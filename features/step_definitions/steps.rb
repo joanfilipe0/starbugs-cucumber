d3e75a97fadc66328a52e2a0bd02f31ca13d07ba
Joan Filipe <joanfilipe0@gmail.com>
2024-04-26 11:48:29 -0300

    Implementando Pages Objects

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,54 +1,35 @@
 Quando('acesso a página principal da starbugs') do
-    visit 'https://starbugs.vercel.app/'
+    @home.open
     
 end
   
 Então('eu devo ver uma lista de cafés disponíveis') do
-    products = all('.coffee-item')
-    expect(products.size).to be > 0
+    expect(@home.coffee_list.size).to be > 0
 
 end
 
 Dado('que estou na página principal da starbugs') do
-    visit 'https://starbugs.vercel.app/'
+    @home.open
 end
 
 Dado('que desejo comprar o seguinte produto') do |table|
     @product = table.hashes[0]
-    # @product_name = table.hashes[0][:product]
-    # @product_price = table.hashes[0][:price]
-    # @delivery_price = table.hashes[0][:delivery]
 end
 
 Quando('inicio a compra desse item') do
-    product = find('.coffee-item',text: @product[:name])
-    product.find('.buy-coffee').click
-
+    @home.buy(@product[:name])
 end
 
 Então('devo ver a página de checkout com os detalhes do produto') do
-    product_title = find('.item-details h1')
-    expect(product_title.text).to eql @product[:name]
-
-    sub_price = find ('.subtotal .sub-price')
-    expect(sub_price.text).to eql @product[:price]
-
-    delivery = find('.delivery-price')
-    expect(delivery.text).to eql @product[:delivery]
-
+    @checkout.assert_product_datails(@product)
 end
 
-Então('o valor total da compra deve ser de {string}') do |total_price|
-    
-    price = find('.total-price')
-    expect(price.text).to eql total_price
-    
-
+Então('o valor total da compra deve ser de {string}') do |total_price| 
+    @checkout.assert_total_price(total_price) 
 end
 
 Então('devo ver um pop-up informando que o produto está indisponível') do
-    popup = find('.swal2-html-container')
-    expect(popup.text).to eql 'Produto indisponível'
-  end
+    @popup.have_text('Produto indisponível')
+end
 
   
